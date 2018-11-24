13a43dd524bccf38d6e626ffb111acf0a4e760fb
JY <kims2606@gmail.com>
2018-11-24 19:45:45 +0900

    대여 서비스 보듈화, 로그 추가, 수정

--- src/kr/jy/book/Main.java
+++ src/kr/jy/book/Main.java
@@ -1,22 +1,17 @@
 package kr.jy.book;
 
-import java.util.Scanner;
-
 import kr.jy.book.controller.BookCt;
 import kr.jy.book.controller.LendCt;
 import kr.jy.book.controller.MemberCt;
-import kr.jy.book.service.BookSv;
-import kr.jy.book.service.MemberSv;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BookSv bookSv = new BookSv();
-        MemberSv memberSv = new MemberSv();
-
         BookCt bookCt = new BookCt();
-        MemberCt memberCt = new MemberCt(memberSv);
+        MemberCt memberCt = new MemberCt();
         LendCt lendCt = new LendCt();
 
         Scanner input = new Scanner(System.in);
@@ -24,14 +19,13 @@ public class Main {
         int actionType = 0;
 
         while(true) {
-
             System.out.print("\n[1]:도서관리  [2]:회원관리  [3]:대여관리  [9]:종료\n>> ");
             actionType = input.nextInt();
             input.nextLine();
 
             //도서관리
             if(actionType == 1) {
-                bookCt.manage(bookSv);
+                bookCt.manage();
 
             //회원 검색
             } else if(actionType == 2) {
@@ -39,7 +33,7 @@ public class Main {
 
             //대여관리
             } else if(actionType == 3) {
-                lendCt.manage(memberSv, bookSv);
+                lendCt.manage();
 
             //종료
             } else if(actionType == 9) {
--- src/kr/jy/book/controller/LendCt.java
+++ src/kr/jy/book/controller/LendCt.java
@@ -1,27 +1,29 @@
 package kr.jy.book.controller;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
+import kr.jy.book.dto.Book;
 import kr.jy.book.dto.Member;
 import kr.jy.book.exception.LendBookException;
 import kr.jy.book.service.BookSv;
+import kr.jy.book.service.LendSv;
 import kr.jy.book.service.MemberSv;
 
 public class LendCt {
 
-    private MemberSv memberSv;
-    private BookSv bookSv;
+    private MemberSv memberSv = MemberSv.getInstance();
+    private BookSv bookSv = BookSv.getInstance();
+    private LendSv lendSv = LendSv.getInstance();
     private Scanner input = new Scanner(System.in);
 
     // 대여
-    public void manage(MemberSv memberSv, BookSv bookSv) {
-        this.memberSv = memberSv;
-        this.bookSv = bookSv;
+    public void manage() {
         int actionType = 0;
 
         while(true) {
-            System.out.print("\n[1]:도서대여  [2]:도서반납 [9]:뒤로가기\n>> ");
+            System.out.print("\n[1]:도서대여  [2]:도서반납  [3]:로그  [9]:뒤로가기\n>> ");
             actionType = input.nextInt();
             input.nextLine();
 
@@ -30,6 +32,9 @@ public class LendCt {
 
             } else if(actionType == 2) {
                 this.getBackBook();
+
+            } else if(actionType == 3) {
+                this.getLogList();
 
             } else if(actionType == 9) {
                 break;
@@ -40,61 +45,83 @@ public class LendCt {
 
     //도서 대여
     private void borrowBook() {
-        System.out.print("\n대여받을 회원 ID를 입력하세요\n>> ");
-
-        Optional<Member> data = memberSv.searchMember(input.nextInt());
-        if(!data.isPresent()) {
+        System.out.print("\n대여받을 회원 ID, 대여할 책 ID를 입력하세요 ex)1,1\n>> ");
+        Optional<Member> memberData = memberSv.searchMember(input.nextInt());
+        if(!memberData.isPresent()) {
             System.out.println("회원이 존재하지 않습니다.");
             return;
         }
 
         System.out.print("\n대여할 책 ID를 입력하세요\n>> ");
-        int bookId = input.nextInt();
-        input.nextLine();
+        Optional<Book> bookData = bookSv.searchBook(input.nextInt());
+        if(!bookData.isPresent()) {
+            System.out.println("책이 존재하지 않습니다.");
+            return;
+        }
 
-
-        try
-        {
-            if (!bookSv.checkLendBook (data.get(), bookId))
-            {
-                // 대여할수 없는 상태
-            }
-
-            bookSv.lendBook(data.get(), bookId);
-
+        try {
+            lendSv.lendBook(memberData.get(), bookData.get());
             System.out.println("대여 완료 되었습니다.");
         }
-        catch (LendBookException e)
-        {
-            if (e.getExceptionType () == LendBookException.LendBookExceptionType.IS_RENTAL)
-                System.out.println("이미 대여중입니다.");
-            else if (e.getExceptionType () == LendBookException.LendBookExceptionType.NO_BOOK)
-                System.out.println("존재 하지 않는 책입니다.");
-            else if (e.getExceptionType () == LendBookException.LendBookExceptionType.NO_MEMBER)
-                System.out.println("존재 하지 않는 회원입니다.");
-            else
-                System.out.println("대여할 수 없습니다.");
+        catch (LendBookException e) {
+            handlerException(e);
         }
     }
 
     //도서 반납
     private void getBackBook() {
         System.out.print("\n반납할 회원 ID를 입력하세요\n>> ");
-
-        Optional<Member> data = memberSv.searchMember(input.nextInt());
-        if(!data.isPresent()) {
+        Optional<Member> memberData = memberSv.searchMember(input.nextInt());
+        if(!memberData.isPresent()) {
             System.out.println("회원이 존재하지 않습니다.");
             return;
         }
 
         System.out.print("\n반납할 책 ID를 입력하세요\n>> ");
-        int bookId = input.nextInt();
-        input.nextLine();
+        Optional<Book> bookData = bookSv.searchBook(input.nextInt());
+        if(!bookData.isPresent()) {
+            System.out.println("책이 존재하지 않습니다.");
+            return;
+        }
 
-        if( bookSv.getBackBook(data.get(), bookId) ) {
+        try {
+            lendSv.getBackBook(memberData.get(), bookData.get());
             System.out.println("반납 완료 되었습니다.");
-        } else {
-            System.out.println("반납할 수 없습니다.");
+
+        } catch(LendBookException e) {
+            handlerException(e);
         }
     }
+
+    /**
+     * 예외 처리
+     * @param e
+     */
+    private void handlerException(LendBookException e) {
+        if (e.getExceptionType () == LendBookException.LendBookExceptionType.IS_RENTAL)
+            System.out.println("이미 대여중입니다.");
+        else if (e.getExceptionType () == LendBookException.LendBookExceptionType.IS_RENTAL_NO)
+            System.out.println("대여중인 책이 아닙니다.");
+        else if (e.getExceptionType () == LendBookException.LendBookExceptionType.NO_BOOK)
+            System.out.println("존재 하지 않는 책입니다.");
+        else if (e.getExceptionType () == LendBookException.LendBookExceptionType.NO_MEMBER)
+            System.out.println("존재 하지 않는 회원입니다.");
+        else
+            System.out.println("대여할 수 없습니다.");
+    }
+
+    /**
+     * 로그 조회
+     */
+    public void getLogList() {
+        List<String> logList = lendSv.getLogList();
+
+        if(logList.size() == 0) {
+            System.out.println("로그가 없습니다.");
+            return;
+        }
+
+        logList.stream().forEach(log -> System.out.println(log));
+    }
+
 }
